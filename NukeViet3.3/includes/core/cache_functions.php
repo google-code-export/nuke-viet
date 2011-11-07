fee2e179632a7da2032d096408c8bd0e47a10b13
vuthao27 <vuthao27@a957d178-3835-f50f-e15a-6e012bba7686>
2011-11-07 17:00:30 +0000

    change cache functions

--- NukeViet3.3/includes/core/cache_functions.php
+++ NukeViet3.3/includes/core/cache_functions.php
@@ -7,123 +7,128 @@
  * @Createdate 1/9/2010, 3:21
  */
 
-if ( ! defined( 'NV_MAINFILE' ) ) die( 'Stop!!!' );
+if (!defined('NV_MAINFILE'))
+    die('Stop!!!');
 
 /**
  * nv_delete_cache()
- * 
+ *
  * @param mixed $pattern
  * @return
  */
-function nv_delete_cache( $pattern )
+function nv_delete_cache($pattern)
 {
-	global $lang_global, $sys_info, $global_config;
- 	if ( $dh = opendir( NV_ROOTDIR . "/" . NV_CACHEDIR ) )
+    global $lang_global, $sys_info, $global_config;
+    if ($dh = opendir(NV_ROOTDIR . "/" . NV_CACHEDIR))
     {
-        while ( ( $file = readdir( $dh ) ) !== false )
+        while (($file = readdir($dh)) !== false)
         {
-        	if ( preg_match( $pattern, $file ) )
-        	{
-				unlink(NV_ROOTDIR . "/" . NV_CACHEDIR . "/" . $file);        		
-			}
+            if (preg_match($pattern, $file))
+            {
+                unlink(NV_ROOTDIR . "/" . NV_CACHEDIR . "/" . $file);
+            }
         }
-        closedir( $dh );			
+        closedir($dh);
     }
 }
 
 /**
  * nv_delete_all_cache()
- * 
+ *
  * @return
  */
 function nv_delete_all_cache()
 {
     $pattern = "/(.*)\.cache/";
-    nv_delete_cache( $pattern );
+    nv_delete_cache($pattern);
 }
 
 /**
  * nv_del_cache_module()
- * 
+ *
  * @return void
  */
-function nv_del_moduleCache( $module_name )
+function nv_del_moduleCache($module_name, $lang = NV_LANG_DATA)
 {
-    $pattern = "/^" . NV_LANG_DATA . "\_" . $module_name . "\_(.*)\.cache$/i";
-    nv_delete_cache( $pattern );
+    $pattern = "/^" . $lang . "\_" . $module_name . "\_(.*)\.cache$/i";
+    nv_delete_cache($pattern);
 }
 
 /**
  * nv_get_cache()
- * 
+ *
  * @param mixed $filename
  * @return
  */
-function nv_get_cache( $filename )
+function nv_get_cache($filename)
 {
-    if ( empty( $filename ) or ! preg_match( "/(.*)\.cache/", $filename ) ) return false;
-    $filename = basename( $filename );
-    if ( ! file_exists( NV_ROOTDIR . "/" . NV_CACHEDIR . "/" . $filename ) ) return false;
+    if (empty($filename) or !preg_match("/(.*)\.cache/", $filename))
+        return false;
+    $filename = basename($filename);
+    if (!file_exists(NV_ROOTDIR . "/" . NV_CACHEDIR . "/" . $filename))
+        return false;
 
-    return nv_gz_get_contents( NV_ROOTDIR . "/" . NV_CACHEDIR . "/" . $filename );
+    return nv_gz_get_contents(NV_ROOTDIR . "/" . NV_CACHEDIR . "/" . $filename);
 }
 
 /**
  * nv_set_cache()
- * 
+ *
  * @param mixed $filename
  * @param mixed $content
  * @return
  */
-function nv_set_cache( $filename, $content )
+function nv_set_cache($filename, $content)
 {
-    if ( empty( $filename ) or ! preg_match( "/(.*)\.cache/", $filename ) ) return false;
-    $filename = basename( $filename );
+    if (empty($filename) or !preg_match("/(.*)\.cache/", $filename))
+        return false;
+    $filename = basename($filename);
 
-    return nv_gz_put_contents( NV_ROOTDIR . "/" . NV_CACHEDIR . "/" . $filename, $content );
+    return nv_gz_put_contents(NV_ROOTDIR . "/" . NV_CACHEDIR . "/" . $filename, $content);
 }
 
 /**
  * nv_db_cache()
- * 
+ *
  * @param mixed $sql
  * @return
  */
-function nv_db_cache( $sql, $key = '', $modname = '' )
+function nv_db_cache($sql, $key = '', $modname = '', $lang = NV_LANG_DATA)
 {
     global $db, $module_name;
 
     $list = array();
 
-    if ( empty( $sql ) ) return $list;
+    if (empty($sql))
+        return $list;
 
-    if ( empty( $modname ) ) $modname = $module_name;
+    if (empty($modname))
+        $modname = $module_name;
 
-    $cache_file = NV_LANG_DATA . "_" . $modname . "_" . md5( $sql ) . "_" . NV_CACHE_PREFIX . ".cache";
-    if ( ( $cache = nv_get_cache( $cache_file ) ) != false )
+    $cache_file = $lang . "_" . $modname . "_" . md5($sql) . "_" . NV_CACHE_PREFIX . ".cache";
+    if (($cache = nv_get_cache($cache_file)) != false)
     {
-        $list = unserialize( $cache );
+        $list = unserialize($cache);
     }
     else
     {
-        if ( ( $result = $db->sql_query( $sql ) ) !== false )
+        if (($result = $db->sql_query($sql)) !== false)
         {
             $a = 0;
-            while ( $row = $db->sql_fetch_assoc( $result ) )
+            while ($row = $db->sql_fetch_assoc($result))
             {
-                $key2 = ( ! empty( $key ) and isset( $row[$key] ) ) ? $row[$key] : $a;
+                $key2 = (!empty($key) and isset($row[$key])) ? $row[$key] : $a;
                 $list[$key2] = $row;
                 ++$a;
             }
 
-            $db->sql_freeresult( $result );
+            $db->sql_freeresult($result);
 
-            $cache = serialize( $list );
-            nv_set_cache( $cache_file, $cache );
+            $cache = serialize($list);
+            nv_set_cache($cache_file, $cache);
         }
     }
 
     return $list;
 }
-
 ?>
